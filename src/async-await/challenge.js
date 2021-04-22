b378c94c0128d5b81f4f847e03521101cdeada0d
Jorge Arias Argüelles <69608367+jorgearguellles@users.noreply.github.com>
2021-04-22 13:16:04 -0500

    Update challenge.js

--- src/async-await/challenge.js
+++ src/async-await/challenge.js
@@ -2,8 +2,10 @@ const fetchData = require("../utils/fetchData");
 const API = "https://rickandmortyapi.com/api/character/";
 
 const anotherFunction = async (url_api) => {
+	
 	try {
 		// const data = await fetchData(`${API}`);
+		
 		const data = await fetchData(url_api);
 		const character = await fetchData(`${API}${data.results[0].id}`);
 		const dimension = await fetchData(character.origin.url);
@@ -11,9 +13,11 @@ const anotherFunction = async (url_api) => {
 		console.log(data.info.count);
 		console.log(character.name);
 		console.log(dimension.dimension);
+		
 	} catch (error) {
 		console.error(error);
 	}
+	
 };
 
 console.log("Before:");
